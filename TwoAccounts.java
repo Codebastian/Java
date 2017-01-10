9e1fa1c86a942220deffb8aaed681c1f0cb7a15d
Codebastian <mrcodebastian@gmail.com>
2017-01-10 10:53:43 -0500

    Update TwoAccounts.java
    This Java program has two accounts which will be able to access via another external window that will open up.

--- TwoAccounts.java
+++ TwoAccounts.java
@@ -4,4 +4,34 @@ public class Authenticator {
 
   public static void main(String args[]) {
   
-  String username
+  String username =
+    JOptionPane.showInputDialog("Username: "); {
+  String password =
+    JOptionPane.showInputDialog("Password: "); 
+  
+    if (
+       username != null &&
+       password != null &&
+      (
+        (username.equals("Sebastian") &&
+         password.equals("Cheetah")) ||
+        (username.equals("David") &&
+         password.equald("Password"))
+        
+     /* The || is the divider between the two accounts, 
+        it tells the program that they are seperate and Sebastian stays with Cheetah
+     */
+        
+        )
+      )
+      
+    { 
+      JOptionPane.showMessageDialog
+        (null, "You have logged into" + username + "'s Account")
+        
+    } else {
+      JOptionPane.showMessaheDialog
+        (null, "Incorrect Username or password");
+    }
+  }
+}
