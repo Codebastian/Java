f0b2ac24778bc0ea21e2f808de2aa55003e613fd
Codebastian <mrcodebastian@gmail.com>
2017-01-10 10:22:32 -0500

    Update Account.java
    This is a simple way to construct a Username and password recognition program.

--- Account.java
+++ Account.java
@@ -1,1 +1,32 @@
+import static java.lang.System.out;
+import java.utill.Scanner;
 
+public class Account {
+  
+  public static void main(String args[]) {
+    Scanner username = new Scanner(System.in);
+    
+    /* This opens the scanner to recognise that the guest user is 
+    *  about to log in
+    */
+    
+    out.print("Username:");
+    String username = keyboard.next();
+  
+    if(username.equals("Sebastian")) {
+      out.print("Password:");
+      String password = keyboard.next();
+      
+      /* If the Username typed in is equal to Sebastian, 
+      *  it will ask for the password of Sebastian (Cheetah)
+      */
+      
+    if(password.equals("Cheetah")) {
+      out.print("Logged in");
+    
+    } else { 
+      out.println(Unknown Username or Password, please verify");
+  }
+       keyboard.close():
+ }
+}
