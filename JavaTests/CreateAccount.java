30764f4d643dcde1f9d2e62ba91c73e403dbdd06
Codebastian <mrcodebastian@gmail.com>
2017-01-12 11:10:57 -0500

    Update CreateAccount.java

--- JavaTests/CreateAccount.java
+++ JavaTests/CreateAccount.java
@@ -1,1 +1,30 @@
+import static java.lang.System.out;
+import java.utill.Scanner;
 
+public static void main(String args[]) {
+
+  int username;
+  int password;
+    
+  /* 
+  */
+    out.print("Set your Username:");
+    String username = keyboard.next();
+  
+      (username.equals(username));
+  
+  
+    out.print("Set your Password:");
+    String username = keyboard.next();
+  
+      (password.equals(password);
+     
+     
+     
+     /* This is test code, this code will not work. 
+     
+     */
+  
+  
+
+}
